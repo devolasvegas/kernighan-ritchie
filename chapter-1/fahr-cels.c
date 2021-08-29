aafb2574252d0fa9dce9fbaabfe730808441209e
Devon Daviau <devondaviau@yahoo.ca>
2021-08-28 21:16:06 -0400

    add heading to output of fahr-cels.c

--- chapter-1/fahr-cels.c
+++ chapter-1/fahr-cels.c
@@ -13,9 +13,11 @@ main()
 	step = 20;	/* step size */
 
 	fahr = lower;
+	
+	printf("Fahr\tCels\n");
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
+		printf("%4.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
